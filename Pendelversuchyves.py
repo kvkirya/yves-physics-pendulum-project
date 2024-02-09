e2a86166e34a27b884c29cb99b8e206e2938b0c7
Kyrill Vovk <sekth.kyrillvovk@gmail.com>
2024-02-09 22:10:51 +0100

    updated main python script to have more subplots

--- Pendelversuchyves.py
+++ Pendelversuchyves.py
@@ -55,17 +55,57 @@ xs_SP = np.array([i for i in range(0,1000)])
 
 
 
+
+
+"""big pendel coordinate assignment"""
+ys_B =np.array([y_berechnung_big_pendel(t*dt) for t in range(262,438)])
+xs_B = np.array([i for i in range(0,176)])
+
+"""big pendel coordinate assignment 2"""
+ys_B2 =np.array([y_berechnung_big_pendel(t*dt) for t in range(262,438)])
+xs_B2 = np.array([i for i in range(278,454)])
+
+"""big pendel coordinate assignment 3"""
+ys_B3 =np.array([y_berechnung_big_pendel(t*dt) for t in range(262,438)])
+xs_B3 = np.array([i for i in range(556,732)])
+
+
+
+
+"""small pendel coordinate assignment"""
+ys_S = np.array([y_berechnung_small_pendel(t*dt) for t in range(0,102)])
+xs_S = np.array([i for i in range(175,277)])
+
+"""small pendel coordinate assignment2"""
+ys_S2 = np.array([y_berechnung_small_pendel(t*dt) for t in range(0,102)])
+xs_S2 = np.array([i for i in range(454,556)])
+
+"""small pendel coordinate assignment3"""
+ys_S3 = np.array([y_berechnung_small_pendel(t*dt) for t in range(0,102)])
+xs_S3 = np.array([i for i in range(732,834)])
+
+
+
+"""creating subplots"""
+fig, axs = plt.subplots(3)
+fig.suptitle('Pendel calculations')
+
 """Plot based on Function"""
-plt.subplot(121)
-plt.plot(xs,ys)
-plt.xlabel("plot based on function")
+axs[0].plot(xs,ys)
 
 """Plots seperated based on long Pendel and short Pendel"""
-plt.subplot(122)
-plt.plot(xs_BP, ys_BP) #Long Pendel
-plt.plot(xs_SP, ys_SP) #Short Pendel
-plt.xlabel("Plot seperating both long and short Pendel swing")
+axs[1].plot(xs_BP, ys_BP) #Long Pendel
+axs[1].plot(xs_SP, ys_SP) #Short Pendel
+
+
+"""Plot combination of the combination of single waves"""
+axs[2].plot(xs_B,ys_B) #First positive Curve
+axs[2].plot(xs_S, ys_S) #First negative Curve
+axs[2].plot(xs_B2,ys_B2) #Second positive Curve
+axs[2].plot(xs_S2, ys_S2) #Second negative Curve
+axs[2].plot(xs_B3,ys_B3) #Third positive Curve
+axs[2].plot(xs_S3, ys_S3) #Third negative Curve
+
 
 """show plots"""
-plt.suptitle("Pendel calculations")
 plt.show()
